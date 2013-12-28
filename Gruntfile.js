c593d03727486f1d9a421585af7b21e7a2ba3ccc
ayan4m1 <andrew@bulletlogic.com>
2013-12-28 13:56:03 -0500

    simplify Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,28 +2,18 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		manifest: grunt.file.readJSON('src/manifest.json'),
-		copy: {
-			main: {
-				files: [
-					{ src: 'src/manifest.json', dest: 'build/manifest.json' },
-					{ src: 'src/jquery.min.js', dest: 'build/jquery.min.js' },
-					{ src: 'src/main.js', dest: 'build/main.js' }
-				]
-			}
-		},
 		compress: {
 			main: {
 				options: {
 					archive: "<%= manifest.name %>-<%= manifest.version %>.zip"
 				},
 				files: [
-					{ expand: true, cwd: 'build/', src: ['**'], dest: '/' }
+					{ expand: true, cwd: 'src/', src: ['**'], dest: '/' }
 				]
 			}
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-compress')
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.registerTask('default', ['copy', 'compress'])
+	grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.registerTask('default', ['compress']);
 };
