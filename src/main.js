5a554c9dbd295a58b6ece5eff10145a58f702b71
ayan4m1 <andrew@bulletlogic.com>
2013-12-28 18:14:24 -0500

    add support for "original series" pages

--- src/main.js
+++ src/main.js
@@ -9,25 +9,43 @@
 // ==/UserScript==
 
 jQuery(function($) {
-	function isShowPage() {
-		return ($('.episodeList li').length > 0);
+	// define a list of page types that we want to match on / insert into
+	var handlers = [
+		{
+			'selector': '.episodeList li',
+			'insert': function() {
+				var elem = $('<span class="mltBtn"><a class="svf-button svfb-silver evo-btn svf-button-inq" style="margin-left:10px"><span class="inr">Random Episode</span></a></span>');
+				elem.on('click', function() {
+					var idx = randomIdx($('.episodeList li').length);
+					$('#e' + idx).trigger('click');
+				});
+				$('#seasonSelector').after(elem);
+			}
+		},
+		{
+			'selector': '.currentSeason .videoRow',
+			'insert': function() {
+				var elem = $('<span class="mltBtn" style="position:relative;top:35px;float:right"><a class="svf-button svfb-silver evo-btn svf-button-inq" style="margin-left:10px"><span class="inr">Random Episode</span></a></span>');
+				elem.on('click', function() {
+					var idx = randomIdx($('.currentSeason .videoRow').length);
+					$('.currentSeason .videoRow:nth-child(' + idx + ')').trigger('click');
+				});
+				$('.video-controls').css('height', '70px');
+				$('.video-controls .video-tabs').after(elem);
+			},
+			'buttonHtml': '<div class="write-btn-wrap"><div class="dp-btn-wrap"><a class="dp-btn">Random Episode</a></div></div>'
+		}
+	];
+
+	// utility function to select a random link
+	function randomIdx(max) {
+		return Math.ceil(Math.random() * max);
 	}
 
-	function createButton() {
-		var elem = $('<span class="mltBtn"><a class="svf-button svfb-silver evo-btn svf-button-inq" style="margin-left:10px"><span class="inr">Random Episode</span></a></span>');
-		elem.on('click', function() {
-			var cnt = $('.episodeList li').length;
-			var idx = Math.ceil(Math.random() * cnt);
-			$('#e' + idx).trigger('click');
-		});
-		return elem;
-	}
-
-	function insertButton() {
-		$('#seasonSelector').after(createButton());
-	}
-
-	if (isShowPage()) {
-		insertButton();
-	}
+	// search the page for each handler and insert if applicable
+	$.each(handlers, function(i, v) {
+		if ($(v.selector).length > 0) {
+			v.insert()
+		}
+	});
 });
